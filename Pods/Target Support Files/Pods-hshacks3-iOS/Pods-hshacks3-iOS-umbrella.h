c9e08f1158f98ebedb5b08b1fc8ed37c4f3a0d4b
Aubhro <aubhrosengupta@gmail.com>
2017-03-25 18:56:18 -0700

    Configure Firebase

---
+++ Pods/Target Support Files/Pods-hshacks3-iOS/Pods-hshacks3-iOS-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_hshacks3_iOSVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_hshacks3_iOSVersionString[];
+
