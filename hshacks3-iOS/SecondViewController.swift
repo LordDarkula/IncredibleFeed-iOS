3958a62e344911cb7e63cff8e6abb237a9c2dfe1
Aubhro <aubhrosengupta@gmail.com>
2017-03-25 18:24:59 -0700

    Initial Commit

---
+++ hshacks3-iOS/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  hshacks3-iOS
+//
+//  Created by Aubhro Sengupta on 3/25/17.
+//  Copyright © 2017 Aubhro Sengupta. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
